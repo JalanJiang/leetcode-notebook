988a11c9d3c539e7e875423c2b516f7949af7352
JalanJiang <448300947@qq.com>
2019-05-24 23:05:37 +0800

    feat(math): 172. 阶乘后的零

--- _book/push.sh
+++ _book/push.sh
@@ -5,5 +5,5 @@ sudo gitbook build
 git add *
 git commit -m "$message"
 git push origin master
-# git subtree push --prefix=_book origin gh-pages
-git push origin `git subtree split --prefix=_book master`:gh-pages --force
+git subtree push --prefix=_book origin gh-pages
+# git push origin `git subtree split --prefix=_book master`:gh-pages --force
