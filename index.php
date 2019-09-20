2f7dabfa5372a6e2acf159bbc2a82ca1484aa97a
JalanJiang <448300947@qq.com>
2019-09-21 00:26:00 +0800

    🌝(*): 伪装最好的语言

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-echo "Hello, World!";
+echo "PHP is the best programming language in the world!";
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def main():
-    print("Life is short, I use Python!")
+    print("Life is short, you need Python.")
 
 if __name__ == '__main__':
     main()
